2786da762b5e62724fdccf0b6188922cdf712ad2
Kaya <hmchen@users.noreply.github.com>
2021-01-25 21:48:20 -0800

    restart

--- javascript/src/test/pages/Home/Home.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import Home from "main/pages/Home/Home";
-
-describe("Home tests", () => {
-  test("renders without crashing", () => {
-    render(<Home />);
-  });
-});
